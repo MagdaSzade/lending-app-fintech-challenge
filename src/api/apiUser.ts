40fecc8f542a29c19c69e3ff50d2212d44bcbed6
Michał Nielipiński <bukaniex@gmail.com>
2020-11-26 11:18:01 +0100

    removed unnecessary dependency

--- src/api/apiUser.ts
+++ src/api/apiUser.ts
@@ -1,6 +1,5 @@
 import {backend} from './backend';
 import {ReagisterNewUserForm} from '../helpers/types';
-import Axios from 'axios';
 
 export const registerNewUser = (data: ReagisterNewUserForm) => {
     return backend.post('/users/register', data);
